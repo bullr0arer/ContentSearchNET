5a537a3517ec4562593480cb1c69dbc7895593c5
Bryan Hutto <BSHutto27@gmail.com>
2022-09-04 17:44:16 -0400

    Add project files.

---
+++ ContentSearchNET/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ContentSearchNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int exit = 0;
+            List<FileInfo> accessList = new List<FileInfo>();
+            while (exit == 0)
+            {
+                accessList.Clear();
+                Console.WriteLine("Specify directory path?");
+                String queryDir = Console.ReadLine();
+                if (queryDir.Equals("close")) { exit = 0; break; }
+                Console.WriteLine("Term to search for?");
+                String queryTerm = Console.ReadLine();
+
+                DirectoryInfo dirInfo = new DirectoryInfo(@queryDir);
+                if (!dirInfo.Exists)
+                {
+                    Console.WriteLine("*no such directory found, please try again*");
+                    continue;
+                }
+                Searcher searcher = new Searcher();
+                searcher.searchDir(queryDir, queryTerm);
+                Stack<FileInfo> matches = searcher.getMatches();
+                int minuend = matches.Count;
+                while (matches.Count > 0) {
+                    accessList.Add(matches.Pop());
+                    int currIndex = minuend - matches.Count - 1;
+                    FileInfo match = accessList[currIndex];
+                    Console.WriteLine(currIndex.ToString() + ": " + match.FullName);
+                }
+                Retry:
+                Console.WriteLine("Examine file? (input file index) Otherwise input \"n\" to start a new search");
+                String resp = Console.ReadLine();
+                if (resp.Equals("n")) { continue; }
+                else if (int.TryParse(resp, out int selection))
+                {
+                    System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(@accessList[selection].FullName) { UseShellExecute = true });
+                    goto Retry;
+                }
+                else { goto Retry; }
+            }
+
+        }
+    }
+}
+
---
+++ ContentSearchNET/Searcher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ContentSearchNET
+{
+    class Searcher
+    {
+        public Stack<FileInfo> fileMatches = new Stack<FileInfo>(0);
+
+
+        public void searchDir(String queryDir, String queryTerm)
+        {
+            //Find the directory 
+            EnumerationOptions options = new EnumerationOptions
+            {
+                IgnoreInaccessible = true
+            };
+            DirectoryInfo dirInfo = new DirectoryInfo(@queryDir);
+
+            //Recursively access entire tree 
+            //TODO: swap to breadth or depth first
+            DirectoryInfo[] subDirs = dirInfo.GetDirectories("*", options);
+            foreach (DirectoryInfo d in subDirs)
+            {
+                searchDir(d.FullName, queryTerm);
+            }
+            //Perform content search 
+            FileInfo[] files = dirInfo.GetFiles("*.txt", options);
+            foreach (FileInfo f in files)
+            {
+                string buffer = null;
+                using (FileStream stream = new
+                    FileStream(f.FullName, FileMode.Open,
+                    FileAccess.Read, FileShare.None))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        buffer = reader.ReadToEnd();
+                    }
+                }
+                if (buffer.Contains(queryTerm))
+                {
+                    this.fileMatches.Push(f);
+                }
+            }
+
+        }
+        public Stack<FileInfo> getMatches() {
+            return fileMatches;
+        }
+    }
+}
